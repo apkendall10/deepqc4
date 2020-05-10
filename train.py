a550bf0b561381a29a870c9804ef628d05b034a7
Amos Kendall <apkendall10@gmail.com>
2020-05-10 10:52:05 -0500

    added data saving

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import agent as a4, gameControls as gc, time, numpy as np, pandas as pd, sys
+import agent as a4, gameControls as gc, time, numpy as np, pandas as pd, sys, os
 
 fpath1 = 'a1'
 fpath2 = 'a2'
@@ -9,8 +9,20 @@ if len(sys.argv) > 1:
 
 start_time = time.time()
 
-mya = a4.agent(fpath=fpath1, debug=False, lr=.01, full_layer = False, use_target = False)
-mya2 = a4.agent(fpath=fpath2, debug=False, lr=.01, use_target = False, advanced_memory = True, full_layer = False)
+path = 'data/train'
+error = True
+fileNum = 0
+while error:
+    fileNum += 1
+    try:
+        os.mkdir(path = path + str(fileNum))
+        error = False
+        print('Writing to ' + path + str(fileNum))
+    except:
+        pass
+
+mya = a4.agent(fpath=None, debug=False, lr=.01, full_layer = False, use_target = False)
+mya2 = a4.agent(fpath=None, debug=False, lr=.01, use_target = False, advanced_memory = True, full_layer = False)
 
 for j in range(rounds):
     print('round', j)
@@ -55,8 +67,8 @@ for j in range(rounds):
 
     data = np.array([y1,y2,z1,z2, playCount])
     df = pd.DataFrame(data.T, columns= ['A1 Wins', 'A2 Wins', 'A1 Loss', 'A2 Loss', 'Play Count'])
-    fnam = 'data/train2/test{}.txt'.format(j+1)
-    #df.to_csv(fnam, index = False)
+    fnam = path + str(fileNum) + '/test{}.txt'.format(j+1)
+    df.to_csv(fnam, index = False)
 
     mya.save_agent(fpath1)
     mya2.save_agent(fpath2)
