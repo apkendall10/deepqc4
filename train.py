256c38d1c2e61d6486b6daed86f2707af67e12e2
Amos Kendall <apkendall10@gmail.com>
2020-05-16 09:14:14 -0500

    move to target training

--- train.py
+++ train.py
@@ -22,8 +22,8 @@ while error and fileNum < 5000:
         pass
 
 
-mya = a4.agent(fpath=fpath1, debug=False, lr=.01, full_layer = False, use_target = False)
-mya2 = a4.agent(fpath=fpath2, debug=False, lr=.01, use_target = False, advanced_memory = True, full_layer = False)
+mya = a4.agent(fpath=fpath1, debug=False, lr=.01, full_layer = False, use_target = True)
+mya2 = a4.agent(fpath=fpath2, debug=False, lr=.01, use_target = True, advanced_memory = True, full_layer = False)
 
 for j in range(rounds):
     print('round', j)
@@ -55,21 +55,15 @@ for j in range(rounds):
     y2 = score[:,1]
     z1 = np.array(loss1) * 100
     z2 = np.array(loss2) * 100
-    if False:
-        plt.scatter(x,y1, label = 'A1 Wins')
-        plt.scatter(x,y2, label = 'A2 Wins')
-        plt.scatter(x,z1, label = 'A1 Loss')
-        plt.scatter(x,z2, label = 'A2 Loss')
-        plt.legend(loc = 'best')
-        plt.xlabel("Epoch")
-        plt.show()
 
     playCount = np.array(playCount)
+
+    mya.save_agent(fpath1)
+    mya2.save_agent(fpath2)
 
     data = np.array([y1,y2,z1,z2, playCount])
     df = pd.DataFrame(data.T, columns= ['A1 Wins', 'A2 Wins', 'A1 Loss', 'A2 Loss', 'Play Count'])
     fnam = path + str(fileNum) + '/test{}.txt'.format(j+1)
     df.to_csv(fnam, index = False)
 
-    mya.save_agent(fpath1)
-    mya2.save_agent(fpath2)
+    
